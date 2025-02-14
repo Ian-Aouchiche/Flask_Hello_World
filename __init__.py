e84b98e628d27b2339c016c80a5ef6efe872d5da
Ian-Aouchiche <124344080+Ian-Aouchiche@users.noreply.github.com>
2025-02-14 10:36:39 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ def exercices():
 
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
