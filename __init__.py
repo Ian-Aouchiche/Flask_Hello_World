d44b268ee4e10c646cba398d74ecd2c563254db2
Ian-Aouchiche <124344080+Ian-Aouchiche@users.noreply.github.com>
2025-02-14 10:33:01 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exercices cliquez <a href='./exercices/'>Ici</a></p>"
-
+    return """<h2>Bonjour tout le monde !</h2>
+              <p>Pour accéder à vos exercices cliquez <a href='./exercices/'>Ici</a></p>
+              <p>Pour accéder à la page de contact cliquez <a href='./contact/'>Ici</a></p>"""
+  
 @app.route('/exercices/')
 def exercices():
     return render_template('exercices.html')  # Assure-toi que ce fichier existe dans "templates/"
